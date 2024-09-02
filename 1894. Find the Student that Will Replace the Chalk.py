3bd19dedf86aa94441ded6b0a09a8d17d3db8e3a
Koh Mingyang <kohmingyang@outlook.com>
2024-09-02 21:33:58 +0800

    Added 1894. Find the Student that Will Replace the Chalk

---
+++ 1894. Find the Student that Will Replace the Chalk.py
@@ -0,0 +1,17 @@
+class Solution:
+    def chalkReplacer(self, chalk: List[int], k: int) -> int:
+        # Reduce the number of loops
+        k %= sum(chalk)
+        used = 0
+        student =  0
+
+        while used + chalk[student % len(chalk)] <= k:
+            used += chalk[student % len(chalk)]
+            student += 1
+        
+        return student % len(chalk)
+    
+"""
+Time complexity: O(n)
+Space complexity: O(1
+)"""
